323c52ab66d692209fc74120dd31316f76bbaa3c
Nikita <kiselov.nikita2013@yandex.ru>
2024-03-07 20:33:56 +0300

    test29

--- app_product/tests/unit/test_product_model.py
+++ app_product/tests/unit/test_product_model.py
@@ -10,42 +10,42 @@ from pydantic import ValidationError
 from app.models.product import Product
 
 product_id: UUID
-ord_id: UUID
-type: str
-customer_info: str
-create_date: datetime
-product: str
+order_id: UUID
+name: str
+brand: str
+price: float
+created_at: datetime
 
 
 def test_product_creation():
     product_id = uuid4()
-    ord_id = uuid4()
-    type = 'test_product_type_1'
-    create_date = datetime.now()
-    product = 'test_product_product_1'
-    customer_info = 'test_customer_info_0'
+    order_id = uuid4()
+    name = 'test_product_type_1'
+    brand = 'test_product_brand_1'
+    price = 10000
+    created_at = datetime.now()
 
-    product = Product(product_id=product_id, ord_id=ord_id, type=type, create_date=create_date, product=product,
-                        customer_info=customer_info)
+    product = Product(product_id=product_id, order_id=order_id, name=name, brand=brand, price=price,
+                        created_at=created_at)
 
-    assert dict(product) == {'product_id': product_id, 'ord_id': ord_id, 'type': type,
-                              'create_date': create_date, 'product': product,
-                              'customer_info': customer_info}
+    assert dict(product) == {'product_id': product_id, 'order_id': order_id, 'name': name,
+                              'brand': brand, 'price': price,
+                              'created_at': created_at}
 
 
 def test_product_date_required():
     with pytest.raises(ValidationError):
         Product(product_id=uuid4(),
-                 ord_id=uuid4(),
-                 type='test_product_type_1',
-                 product='test_product_product_1',
-                 customer_info='test_customer_info_0')
+                 order_id=uuid4(),
+                 name='test_product_name_1',
+                 brand='test_product_brand_1',
+                 price=10000)
 
 
-def test_product_ord_id_required():
+def test_product_order_id_required():
     with pytest.raises(ValidationError):
         Product(product_id=uuid4(),
-                 type='test_product_type_1',
-                 create_date=datetime.now(),
-                 product='test_product_product_1',
-                 customer_info='test_customer_info_0')
+                 name='test_product_name_1',
+                 brand='test_product_brand_1',
+                 price=10000,
+                 created_at=datetime.now())
